363ab7add124cdcd2b06d0c4b812babcf707bb4e
Charles-HL <61110764+Charles-HL@users.noreply.github.com>
2024-10-03 23:59:05 +0200

    do not require authentication for health check

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -3,6 +3,11 @@ const logger = require('../config/logger');
 const AUTH_TOKEN = process.env.AUTH_TOKEN;
 
 function authMiddleware(req, res, next) {
+  // do not require authentication for health check
+  if (req.path === '/health') {
+    next();
+    return;
+  }
   const authHeader = req.headers.authorization;
   if (authHeader && authHeader === `Bearer ${AUTH_TOKEN}`) {
     next();
