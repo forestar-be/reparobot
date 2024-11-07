6e52a6dfbf91fab543c4d28ea87785f303e82b5f
Diagathe Josué <40642296+Hocoh@users.noreply.github.com>
2024-11-07 22:24:33 +0100

    Development 2 (#5)
    * Google Analytics mises à jour
    
    * mises à jour outils
    
    * mises à jour outils
    
    * mises à jour outils

--- frontend/public-site/src/pages/ROICalculatorPage.tsx
+++ frontend/public-site/src/pages/ROICalculatorPage.tsx
@@ -1,6 +1,7 @@
 // src/pages/ROICalculatorPage.js
 import React from 'react';
-import ROICalculator from '../components/tool/ROICalculator/index.tsx';
+import ROICalculator from '../components/tool/ROICalculator/ROICalculator';
+
 
 const ROICalculatorPage = () => {
   return (
