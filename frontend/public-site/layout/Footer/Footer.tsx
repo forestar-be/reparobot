00f0d1b162069222435327688d8d54edd2f2b992
Forestar <reparobot@gmail.com>
2024-11-25 00:50:13 +0100

    Update Footer.tsx (#18)

--- frontend/public-site/layout/Footer/Footer.tsx
+++ frontend/public-site/layout/Footer/Footer.tsx
@@ -19,25 +19,24 @@ const Footer = (): JSX.Element => {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
-        <Box sx={{ marginBottom: '20px', textAlign: 'center' }}>
-          <Typography
-            align="center"
-            variant="subtitle2"
-            color={theme.palette.text.secondary}
-            gutterBottom
-            sx={{ marginTop: '25px' }}
-          >
-            Copyright &copy; {new Date().getFullYear()} {footer.copyright}.
-          </Typography>
-          <Typography
-            align="center"
-            variant="subtitle2"
-            color={theme.palette.text.secondary}
-            gutterBottom
-          >
-            TVA {footer.TVA}
-          </Typography>
-        </Box>
+       <Box sx={{ marginBottom: '20px', textAlign: 'center' }}>
+  <Typography
+    align="center" 
+    color={theme.palette.text.secondary}
+    gutterBottom
+    sx={{ marginTop: '25px', fontSize: '0.95em' }}
+  >
+    Copyright &copy; {new Date().getFullYear()} {footer.copyright}.
+  </Typography>
+  <Typography
+    align="center" 
+    color={theme.palette.text.secondary}
+    gutterBottom
+    sx={{ fontSize: '0.95em' }}
+  >
+    TVA {footer.TVA}
+  </Typography>
+</Box>
       </Grid>
     </Grid>
   );
